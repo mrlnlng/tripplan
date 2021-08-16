2d7d3543cc373bfdab2224ba4feeba880e90784e
Marlene Liang <marlene.liang@hotmail.com>
2021-08-16 14:49:49 -0400

    Updates

---
+++ precache-manifest.d35713adea39f36ae36fe934b10b69ad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd023f737e67528afdff10882b0d807",
+    "url": "/tripplan/index.html"
+  },
+  {
+    "revision": "d06ac113e191ee754f13",
+    "url": "/tripplan/static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "bbdb34689599889e9b42",
+    "url": "/tripplan/static/css/main.98879411.chunk.css"
+  },
+  {
+    "revision": "d06ac113e191ee754f13",
+    "url": "/tripplan/static/js/2.4176f4ee.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/tripplan/static/js/2.4176f4ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbdb34689599889e9b42",
+    "url": "/tripplan/static/js/main.d6b1ca2e.chunk.js"
+  },
+  {
+    "revision": "65c568d2c474c791367e",
+    "url": "/tripplan/static/js/runtime-main.74a7625f.js"
+  },
+  {
+    "revision": "b1a603d3754e3dca2b21a2f804315a90",
+    "url": "/tripplan/static/media/background.b1a603d3.jpg"
+  },
+  {
+    "revision": "50443b6cfd00a02aaa396ed7a0a7f592",
+    "url": "/tripplan/static/media/header.50443b6c.png"
+  },
+  {
+    "revision": "ae2789cae11fcd44cb2158c1e0296182",
+    "url": "/tripplan/static/media/tripplanBlue.ae2789ca.png"
+  }
+]);
